b3cc1796031b05bd7602fa585ab67379f89914a7
William Yang <williamyang2017@gmail.com>
2023-11-23 06:16:14 +1100

    Updated simple demo website

--- static/js/PacketDecoder.js
+++ static/js/PacketDecoder.js
@@ -2,10 +2,16 @@ class PacketDecoder {
     constructor() {
         this.on_dht11_reading = new Set(); // (humidity, temperature) => {}
         this.on_dht11_error = new Set();  // (error_code) => {}
+        this.on_led_reading = new Set(); // (values: Uint8Array) => {}
+        this.on_pc_status = new Set();  // (is_on) => {}
+        this.on_pc_cmd_result = new Set(); // (command, code) => {}
     }
 
     on_packet = (packet) => {
+        const LED_ID = 1;
+        const PC_ID = 2;
         const DHT11_ID = 3;
+
         if (packet.length < 1) {
             console.error(`Unknown packet: ${packet}`);
             return;
@@ -14,13 +20,80 @@ class PacketDecoder {
         let id = packet[0];
         let data = packet.slice(1);
         switch (id) {
-        case DHT11_ID: 
-            this._on_dht11(data);
-            break;
+        case LED_ID:   this._on_led(data); break;
+        case PC_ID:    this._on_pc_controls(data); break;
+        case DHT11_ID: this._on_dht11(data); break;
         default:
             console.error(`Unknown packet id=${id}, data=${data}`);
             break;
         }
+    }
+
+    _on_pc_controls = (data) => {
+        const PC_POWER_OFF_CMD = 1;
+        const PC_POWER_ON_CMD = 2;
+        const PC_RESET_CMD = 3;
+        const PC_GET_STATUS_CMD = 4;
+
+        if (data.length < 2) {
+            console.error(`Insufficient PC packet length data=${data}`);
+            return;
+        }
+
+        let cmd = data[0];
+        let value = data[1];
+
+        switch (cmd) {
+        case PC_POWER_OFF_CMD:
+        case PC_POWER_ON_CMD:
+        case PC_RESET_CMD:
+            for (let listener of this.on_pc_cmd_result) {
+                listener(cmd, value);
+            }
+            break;
+        case PC_GET_STATUS_CMD:
+            for (let listener of this.on_pc_status) {
+                listener(value === 1);
+            }
+            break;
+        default:
+            console.error(`Unknown PC packet cmd=${cmd}, data=${data}`);
+            break;
+        }
+    }
+
+    _on_led = (data) => {
+        const LED_SET_ID = 1;
+        const LED_GET_ID = 2;
+        if (data.length < 1) {
+            console.error(`Insufficient LED packet length data=${data}`);
+            return;
+        }
+
+        let id = data[0]; 
+        if (id === LED_SET_ID) {
+            return;
+        }
+
+        if (id === LED_GET_ID) {
+            if (data.length < 2) {
+                console.error(`Insufficient LED packet length for LED_GET data=${data}`);
+                return;
+            }
+            let total_pins = data[1];
+            let total_packet_length = total_pins + 2;
+            if (data.length !== total_packet_length) {
+                console.error(`Mismatch between expected (${total_packet_length}) and actual (${data.length}) packet length`);
+                return;
+            }
+            let values = data.slice(2);
+            for (let listener of this.on_led_reading) {
+                listener(values);
+            }
+            return;
+        }
+
+        console.error(`Unknown LED packet id=${id}, data=${data}`);
     }
 
     _on_dht11 = (data) => {
--- static/js/common.js
+++ static/js/common.js
@@ -43,10 +43,13 @@ let bind_dht11 = (app) => {
     let status_elem = document.getElementById("dht11_status");
 
     const DHT11_ID = 3;
+    let refresh_dht11_data = () => {
+        app.send_ws_data(new Uint8Array([DHT11_ID]));
+    };
 
     refresh_elem.addEventListener("click", ev => {
         ev.preventDefault();
-        app.send_ws_data(new Uint8Array([DHT11_ID]));
+        refresh_dht11_data();
     });
 
     app.packet_decoder.on_dht11_reading.add((humidity, temperature) => {
@@ -58,11 +61,167 @@ let bind_dht11 = (app) => {
     app.packet_decoder.on_dht11_error.add((error_code) => {
         status_elem.innerText = `Error(${error_code})`;
     });
+
+    app.on_connection_change.add((state) => {
+        if (state == WebSocket.OPEN) {
+            refresh_dht11_data();
+        }
+    });
+
+    setInterval(() => {
+        refresh_dht11_data();
+    }, 1000);
+}
+
+let bind_led_controls = (app) => {
+    let led_controls_elem = document.getElementById("led_controls");
+    let led_get_all_elem = document.getElementById("led_get_all");
+    let led_set_all_elem = document.getElementById("led_set_all");
+
+    const LED_ID = 1;
+    const LED_SET_CMD = 1;
+    const LED_GET_CMD = 2;
+
+    let slider_elems = [];
+
+    let set_led = (index, value) => {
+        let data = new Uint8Array([LED_ID, LED_SET_CMD, index, value]);
+        app.send_ws_data(data);
+    };
+
+    let set_all_leds = () => {
+        let data = [LED_ID, LED_SET_CMD];
+        let index = 0;
+        for (let slider of slider_elems) {
+            let value = slider.value;
+            data.push(index);
+            data.push(Number(value));
+            index++;
+        }
+        let buffer = new Uint8Array(data);
+        app.send_ws_data(buffer);
+    };
+
+    let get_all_leds = () => {
+        let data = new Uint8Array([LED_ID, LED_GET_CMD]);
+        app.send_ws_data(data);
+    };
+
+    let create_led_control = (index, value) => {
+        let row = document.createElement("tr");
+        let col_0 = document.createElement("td");
+        col_0.innerText = `${index}`;
+
+        let col_1 = document.createElement("td");
+        let slider = document.createElement("input");
+        slider.type = "range";
+        slider.min = "0";
+        slider.max = "127";
+        slider.step = "1";
+        slider.value = String(value);
+
+        col_1.appendChild(slider);
+        row.appendChild(col_0);
+        row.appendChild(col_1);
+        led_controls_elem.appendChild(row);
+        slider_elems.push(slider);
+
+        slider.addEventListener("input", (ev) => {
+            let value = Number(slider.value);
+            set_led(index, value); 
+        });
+    };
+
+    app.packet_decoder.on_led_reading.add((values) => {
+        let total_leds = values.length;
+        if (total_leds > slider_elems.length) {
+            let old_length = slider_elems.length;
+            for (let i = old_length; i < total_leds; i++) {
+                create_led_control(i, values[i]);
+            }
+        }
+        for (let i = 0; i < total_leds; i++) {
+            let slider_elem = slider_elems[i];
+            slider_elem.value = values[i];
+        }
+    });
+
+    led_set_all_elem.addEventListener("click", (ev) => {
+        ev.preventDefault();
+        set_all_leds();
+    });
+
+    led_get_all_elem.addEventListener("click", (ev) => {
+        ev.preventDefault();
+        get_all_leds();
+    });
+
+    app.on_connection_change.add((state) => {
+        if (state == WebSocket.OPEN) {
+            get_all_leds();
+        }
+    });
+}
+
+let bind_pc_controls = (app) => {
+    let button_power_off_elem = document.getElementById("pc_power_off");
+    let button_power_on_elem = document.getElementById("pc_power_on");
+    let button_reset_elem = document.getElementById("pc_reset");
+    let button_get_status_elem = document.getElementById("pc_get_status");
+    let text_status_elem = document.getElementById("pc_status");
+
+    const PC_ID = 2;
+    const PC_POWER_OFF_CMD = 1;
+    const PC_POWER_ON_CMD = 2;
+    const PC_RESET_CMD = 3;
+    const PC_GET_STATUS_CMD = 4;
+    
+    let send_power_off = () => app.send_ws_data(new Uint8Array([PC_ID, PC_POWER_OFF_CMD]));
+    let send_power_on = () => app.send_ws_data(new Uint8Array([PC_ID, PC_POWER_ON_CMD]));
+    let send_reset = () => app.send_ws_data(new Uint8Array([PC_ID, PC_RESET_CMD]));
+    let send_get_status = () => app.send_ws_data(new Uint8Array([PC_ID, PC_GET_STATUS_CMD]));
+
+    button_power_off_elem.addEventListener("click", (ev) => {
+        ev.preventDefault();
+        send_power_off();
+    });
+
+    button_power_on_elem.addEventListener("click", (ev) => {
+        ev.preventDefault();
+        send_power_on();
+    });
+
+    button_reset_elem.addEventListener("click", (ev) => {
+        ev.preventDefault();
+        send_reset();
+    });
+
+    button_get_status_elem.addEventListener("click", (ev) => {
+        ev.preventDefault();
+        send_get_status();
+    });
+
+    app.packet_decoder.on_pc_status.add((is_on) => {
+        let message = is_on ? "On" : "Off";
+        text_status_elem.innerText = `Status: ${message}`;
+    });
+
+    app.packet_decoder.on_pc_cmd_result.add((command, code) => {
+        console.log(`PC command=${command} got code=${code}`);
+    });
+
+    app.on_connection_change.add((state) => {
+        if (state == WebSocket.OPEN) {
+            send_get_status();
+        }
+    });
 }
 
 let bind_app = (app) => {
     bind_connect_button(app);
     bind_dht11(app);
+    bind_led_controls(app);
+    bind_pc_controls(app);
     app.notify_ws_state(WebSocket.CLOSED);
     app.open_websocket(DEFAULT_WS_URL);
 }
